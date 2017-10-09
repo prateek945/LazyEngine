7d5fad9a50c472f01087a8350a91990a1ce28cf2
Prateek Marathe <prateekmarathe594@gmail.com>
2017-10-08 23:39:33 -0700

    textures are a pain

--- LazyEngine/MeshDataCPU/TextureCPU/Texture.h
+++ LazyEngine/MeshDataCPU/TextureCPU/Texture.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <D3D11.h>
 #include <WICTextureLoader.h>
+#include "../../Memory/Handle.h"
 //#include <D3D11tex.h>
 namespace LE {
 	
@@ -12,6 +13,7 @@ namespace LE {
 		ID3D11ShaderResourceView* m_textureView;
 		ID3D11Device* m_device;
 		ID3D11DeviceContext* m_context;
+		Handle *h_texturedata;
 	public:
 		Texture(ID3D11Device* device, ID3D11DeviceContext* context, std::string Path, std::string FileName);
 		Texture(ID3D11Device* context);
