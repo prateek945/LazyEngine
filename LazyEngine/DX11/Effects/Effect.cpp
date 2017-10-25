7bae9d5cdd056917aa9d8b1ba7a490db3d398260
Prateek Marathe <prateekmarathe594@gmail.com>
2017-10-24 19:11:16 -0700

    effect.h

---
+++ LazyEngine/DX11/Effects/Effect.cpp
@@ -0,0 +1,56 @@
+#include "Effect.h"
+
+namespace LE {
+	Effect::Effect() :m_ShaderBase(""),m_EffectName("") ,m_vertexShader(NULL), m_pixelShader(NULL) {}
+
+	Effect::Effect(const char* base,const char* name) : m_vertexShader(NULL), m_pixelShader(NULL) {
+		strncpy(m_ShaderBase, base, 256);
+		strncpy(m_EffectName, name, 256);
+	}
+
+	void Effect::setEffectBase(const char* base) {
+		strncpy(m_ShaderBase, base, 256);
+	}
+
+	void Effect::getEffectBase(char* &base) {
+		strncpy(base, m_ShaderBase, 256);
+	}
+
+	void Effect::setEffectName(const char* name) {
+		strncpy(m_EffectName, name, 256);
+	}
+
+	void Effect::getEffectName(char* &name) {
+		strncpy(name, m_EffectName, 256);
+	}
+
+	ID3D11VertexShader* Effect::getVertexShader() {
+
+		return m_vertexShader;
+
+	}
+
+	ID3D11PixelShader* Effect::getPixelShader() {
+
+		return m_pixelShader;
+
+	}
+	ID3D11InputLayout* Effect::getInputLayout() {
+
+		return m_inputLayout;
+	}
+	void Effect::setVertexShader(ID3D11VertexShader* vs) {
+
+		m_vertexShader = vs;
+
+	}
+	void Effect::setPixelShader(ID3D11PixelShader* ps) {
+
+		m_pixelShader = ps;
+	}
+	void Effect::setInputLayout(ID3D11InputLayout* il) {
+
+		m_inputLayout = il;
+	}
+	
+};
--- LazyEngine/DX11/Effects/Effect.h
+++ LazyEngine/DX11/Effects/Effect.h
@@ -4,6 +4,28 @@
 
 #include <d3d11.h>
 namespace LE {
-	char m_EffectName[256];
-	ID3D11VertexShader *
+	class Effect {
+	
+		char m_EffectName[256];
+		char m_ShaderBase[256];
+		ID3D11VertexShader *m_vertexShader;
+		ID3D11PixelShader *m_pixelShader;
+		ID3D11InputLayout *m_inputLayout;
+
+	public:
+		Effect();
+		Effect(const char* base,const char* name);
+		void setEffectBase(const char* base);
+		void getEffectBase(char* &base);
+		void setEffectName(const char* name);
+		void getEffectName(char* &name);
+		ID3D11VertexShader* getVertexShader();
+		ID3D11PixelShader* getPixelShader();
+		ID3D11InputLayout* getInputLayout();
+		void setVertexShader(ID3D11VertexShader* vs);
+		void setPixelShader(ID3D11PixelShader* ps);
+		void setInputLayout(ID3D11InputLayout* il);
+		
+	};
+
 };
